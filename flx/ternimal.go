449a560988b35108fbeabd5da8f3027c0bca01ec
EricZhou <1413507308@qq.com>
2019-03-21 12:23:26 +0800

    brofist

--- flx/ternimal.go
+++ flx/ternimal.go
@@ -3,6 +3,7 @@ package flx
 import (
 	"bufio"
 	"github.com/dejavuzhou/felix/models"
+	"github.com/fatih/color"
 	"golang.org/x/crypto/ssh"
 	"io"
 	"os"
@@ -19,6 +20,7 @@ type SSHTerminal struct {
 	stdin              io.Writer
 	stderr             io.Reader
 	Password           string
+	LoginUser          string
 	EnableSudoPassword bool
 }
 
@@ -36,6 +38,7 @@ func RunSshTerminal(h *models.Machine, sudoMode bool) error {
 	s := SSHTerminal{
 		Session:            session,
 		Password:           h.Password,
+		LoginUser:          h.User,
 		EnableSudoPassword: sudoMode,
 	}
 	return s.interactiveSession()
@@ -57,12 +60,12 @@ func enableSudoPassword(t *SSHTerminal) {
 			line = ""
 			continue
 		}
-		if len(line) >= sudoPrefixLen && strings.HasPrefix(line, sudoPrefix) && strings.HasSuffix(line, sudoSuffix) {
+		if len(line) >= sudoPrefixLen && strings.HasPrefix(line, sudoPrefix) && strings.HasSuffix(line, sudoSuffix) && strings.Contains(line, t.LoginUser) {
 			_, err = t.stdin.Write([]byte(t.Password + "\n"))
 			if err != nil {
 				break
 			}
-			os.Stdout.Write([]byte("Felix has input the password for you, No thanks."))
+			color.Green("\r\nFelix has automatically input password for %s", color.BlueString(t.LoginUser))
 		}
 	}
 }
